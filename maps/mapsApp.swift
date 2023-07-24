ede7e2d1b40ca9b0f612bdf86665457c50e25231
PAUL MCCORMICK <COMICALS069@ICLOUD.COM>
2023-07-24 17:09:18 +0100

    Initial Commit

---
+++ maps/mapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapsApp.swift
+//  maps
+//
+//  Created by Paul Mccormick on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct mapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
